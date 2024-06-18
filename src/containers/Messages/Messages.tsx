1ffbc5c81992760c64d1ee6231dabd28227c3698
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-06-18 17:37:41 +0600

    Added the FetchMessages function and rendered it using the Post component

--- src/containers/Messages/Messages.tsx
+++ src/containers/Messages/Messages.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import Post from '../../components/Post/Post';
 
 interface Props {
   author: string;
@@ -9,11 +10,52 @@ interface Props {
 
 const Messages: React.FC = () => {
   const [messages, setMessages] = useState<Props[]>([]);
-  // Другие состояния и логика
+  const [data, setData] = useState('');
+
+
+  const url  = 'http://146.185.154.90:8000/messages';
+
+  let interval: number | null = null;
+
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch(url + '?datetime=' + data);
+      if (response.ok) {
+        const fetchedMessages = await response.json();
+        setMessages(fetchedMessages);
+        if (fetchedMessages.length > 0) {
+          setData(fetchedMessages[fetchedMessages.length - 1].datetime);
+        }
+      }
+    } catch (error) {
+      console.error('Ошибка при получении сообщений:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    void fetchMessages();
+
+    interval = setInterval(fetchMessages, 2000);
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, []);
+
 
   return (
     <div className="inner-container">
-      Чат
+      <div className="posts">
+        {messages.map((post) => (
+          <Post
+            key={post._id}
+            author={post.author}
+            date={(post.datetime)}
+            text={post.message}
+          />
+        ))}
+      </div>
     </div>
   );
 };
